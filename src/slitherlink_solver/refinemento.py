03123ea46a182cde9f5c74a3293ce94686f2b3e2
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-08-01 11:42:00 -0300

    feat: implementa geração de cláusula de bloqueio para sub-ciclos

--- src/slitherlink_solver/refinemento.py
+++ src/slitherlink_solver/refinemento.py
@@ -27,3 +27,19 @@ def detectar_componentes_conexos(arestas_ativas):
     G = nx.Graph()
     G.add_edges_from(arestas_ativas)
     return list(nx.connected_components(G))
+
+
+def gerar_clausula_bloqueio(componente, arestas_ativas, var_por_aresta):
+    clausula = []
+
+    arestas_componente = [
+        aresta
+        for aresta in arestas_ativas
+        if aresta[0] in componente and aresta[1] in componente
+    ]
+
+    for aresta in arestas_componente:
+        variavel_sat = var_por_aresta[aresta]
+        clausula.append(-variavel_sat)
+
+    return clausula
